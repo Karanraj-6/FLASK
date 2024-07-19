10deb71509c04f522ce91a17d3b92bf7cc2200da
Karanraj <karanraj3056@gmail.com>
2024-07-19 23:53:10 +0530

    inheritance

--- inheritance/app.py
+++ inheritance/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route("/home")
 def home():
-    return "Hello, World!, hy..."
+    return "this is home page"
+
+@app.route("/passed/<sname>/<int:smarks>")
+def passed(sname,smarks):
+    return f"congrats {sname} !your passed with {smarks} marks"
+
+@app.route("/failed/<sname>/<int:smarks>")
+def failed(sname,smarks):
+    return f"sorry {sname}! your failed with {smarks} marks"
+
+
+@app.route("/home/<name>/<int:marks>")
+def eval(name,marks):
+    if (marks >= 30):
+        time.sleep(1)
+        return redirect(url_for('passed',sname=name,smarks=marks))
+    else:
+        time.sleep(1)
+        return redirect(url_for('failed',sname=name,smarks=marks))
+
 
 if __name__ == "__main__":
-    print("Starting Flask app...")
     app.run(debug=True)
