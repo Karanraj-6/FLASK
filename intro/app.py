823a0511ecba07623a2fd4ee49d699a93e36dd73
Karanraj <karanraj3056@gmail.com>
2024-07-19 21:04:55 +0530

    commit

--- intro/app.py
+++ intro/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return " first"
-    
-    
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    print("Starting Flask app...")
+    app.run(debug=True)
