c9bf1336716d90c2ed21a3f9351fd2081cf5bba4
Karanraj <karanraj3056@gmail.com>
2024-07-20 18:25:27 +0530

    3_dynamic_urls

--- 3_dynamic_urls/app.py
+++ 3_dynamic_urls/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,url_for
+from employee_data  import employees_data
 
 app= Flask(__name__)
 
@@ -11,5 +12,14 @@ def home():
 def about():
     return render_template("about.html",title="about")
 
+@app.route('/evaluate/<int:num>')
+def evaluate(num):
+    return render_template("evaluate.html",title="Evaluate",number=num)
+
+@app.route("/employee")
+def employee():
+    return render_template("employee.html",title="employee",emps=employees_data)
+    
+    
 if __name__ == "__main__":
     app.run(debug='True')
---
+++ 3_dynamic_urls/employee_data.py
@@ -0,0 +1,17 @@
+employees_data={
+    1:{
+        "name":"jack",
+        "positon":"salesperson",
+        "salary":50000
+    },
+    2:{
+        "name":"alex",
+        "positon":"manager",
+        "salary":100000
+    },
+    3: {
+        "name":"rose",
+        "positon" : "salesperson",
+        "salary":50000
+    }
+}
