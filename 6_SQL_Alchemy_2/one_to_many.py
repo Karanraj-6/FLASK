e1aad7dfecf6435cfb0e6c7644050872864289d2
Karanraj <karanraj3056@gmail.com>
2024-07-28 01:11:14 +0530

    update

--- 6_SQL_Alchemy_2/one_to_many.py
+++ 6_SQL_Alchemy_2/one_to_many.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///employees_db.db"
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///ipl_db.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"]=False
            
 db=SQLAlchemy(app)           
